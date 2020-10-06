313297104edb97c952317de9c4d20c9794013d98
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-06 17:08:15 -0600

    Create a model using sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require_relative 'config/environment'
+#gives us the ability to reference the 
+#TextAnalyzer class and invoke its new method
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
@@ -8,7 +10,9 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    #take the data from params[:user_text] (in the post '/' do route) 
+    #and feed it into a new instance of the TextAnalyzer clas
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :results
   end
 end
